9e1f2e6db18d57857c799c2a926120586b2eca36
alicimsamil <alicimsamil@gmail.com>
2023-06-12 21:52:48 +0300

    create buildSrc module

---
+++ buildSrc/src/main/java/com/alicimsamil/buildsrc/ClassPaths.kt
@@ -0,0 +1,11 @@
+/**
+ * This class contains project level dependencies paths.
+ */
+object ClassPaths {
+    const val kotlinJvm = "org.jetbrains.kotlin.jvm"
+    const val kotlinGradlePath = "org.jetbrains.kotlin.android"
+    const val androidLibraryPath = "com.android.library"
+    const val androidApplicationPath = "com.android.application"
+    const val navigationPath = "androidx.navigation.safeargs"
+    const val hiltPath = "com.google.dagger.hilt.android"
+}
---
+++ buildSrc/src/main/java/com/alicimsamil/buildsrc/DependencyHandlerExt.kt
@@ -0,0 +1,64 @@
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+/**
+ * This function adds implementation dependencies sequentially.
+ */
+fun DependencyHandler.implementation(list: List<String>?) {
+    list?.forEach { dependency ->
+        add("implementation", dependency)
+    }
+}
+
+/**
+ * This function adds androidTestImplementation dependencies sequentially.
+ */
+fun DependencyHandler.androidTestImplementation(list: List<String>?) {
+    list?.forEach { dependency ->
+        add("androidTestImplementation", dependency)
+    }
+}
+
+/**
+ * This function adds testImplementation dependencies sequentially.
+ */
+fun DependencyHandler.testImplementation(list: List<String>?) {
+    list?.forEach { dependency ->
+        add("testImplementation", dependency)
+    }
+}
+
+/**
+ * This function adds debugImplementation dependencies sequentially.
+ */
+fun DependencyHandler.debugImplementation(list: List<String>?) {
+    list?.forEach { dependency ->
+        add("debugImplementation", dependency)
+    }
+}
+
+/**
+ * This function adds releaseImplementation dependencies sequentially.
+ */
+fun DependencyHandler.releaseImplementation(list: List<String>?) {
+    list?.forEach { dependency ->
+        add("releaseImplementation", dependency)
+    }
+}
+
+/**
+ * This function adds annotationProcessor dependencies sequentially.
+ */
+fun DependencyHandler.annotationProcessor(list: List<String>) {
+    list.forEach { dependency ->
+        add("annotationProcessor", dependency)
+    }
+}
+
+/**
+ * This function adds kapt dependencies sequentially.
+ */
+fun DependencyHandler.kapt(list: List<String>) {
+    list.forEach { dependency ->
+        add("kapt", dependency)
+    }
+}
